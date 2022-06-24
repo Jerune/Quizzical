fff783793eebbf35c809eff215757300269dec1f
Jeroen van de Sanden <vandesanden@hotmail.com>
2022-06-24 14:40:13 +0200

    Transform CSS to SCSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./styles/main.css";
 import App from "../src/pages/App/App.js";
 import reportWebVitals from "./reportWebVitals";
 
