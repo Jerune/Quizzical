28e817a1cf8ec59f78c89436a3912e03b061f967
Jeroen van de Sanden <vandesanden@hotmail.com>
2022-06-24 14:33:41 +0200

    feat: Initialisation Project Base
    Install Create React App, React Router, Sass, Font Awesome, EsLint, Prettier and configure for a base master

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "../src/pages/App/App.js";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// @ts-ignore
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
