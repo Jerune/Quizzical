964b93822f87e29ef3fd114d6eb75ce28d13e793
Jeroen van de Sanden <vandesanden@hotmail.com>
2022-07-05 15:48:46 +0200

    feat: Homepage Complete
    Added background elements, components, styling and events to home component

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import Home from "../Home/Home";
 import Quiz from "../Quiz/Quiz";
+import bulbTop from "../../assets/yellow-balloon.png";
+import bulbBottom from "../../assets/blue-balloon.png";
 
 export default function App() {
   const [isHomescreen, setHomeScreen] = useState(true);
@@ -8,6 +10,12 @@ export default function App() {
   return (
     <main className="main">
       {isHomescreen ? <Home onHome={setHomeScreen} /> : <Quiz />}
+      <img src={bulbTop} className="bulb top" alt="design-balloon-top" />
+      <img
+        src={bulbBottom}
+        className="bulb bottom"
+        alt="design-balloon-bottom"
+      />
     </main>
   );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 export default function Home({ onHome }) {
   return (
-    <main className="main">
+    <>
       <h1>Quizzical</h1>
       <p>Dare you answer or do you fail?</p>
-      <button onClick={() => onHome(false)}>Risk your life</button>
-    </main>
+      <button className="buttonHome" onClick={() => onHome(false)}>
+        I'm a Daredevil
+      </button>
+    </>
   );
 }
