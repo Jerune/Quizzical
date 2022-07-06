21c9a5bde40f86e703aacc3c1139ef5e0eb43163
Jeroen van de Sanden <vandesanden@hotmail.com>
2022-07-06 09:21:37 +0200

    Implement fetch of data

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -1,3 +1,10 @@
-export default function Question() {
-  return <div>Question</div>;
+export default function Question({ data }) {
+  return (
+    <div>
+      <h2>{data.question}</h2>
+      <ul>
+        <li>{data.correct_answer}</li>
+      </ul>
+    </div>
+  );
 }
--- src/pages/Quiz/Quiz.jsx
+++ src/pages/Quiz/Quiz.jsx
@@ -1,3 +1,42 @@
+import { useEffect, useState } from "react";
+import Question from "../../components/Question/Question";
+
 export default function Quiz() {
-  return <div>Quiz</div>;
+  const [isActive, setActiveState] = useState(true);
+  const [questions, setQuestions] = useState([]);
+  const [dataIsLoading, setDataLoading] = useState(false);
+
+  console.log(questions);
+
+  useEffect(() => {
+    questions.length === 0 && getQuestions();
+    !isActive && getQuestions();
+  }, [questions, isActive]);
+
+  async function getQuestions() {
+    setDataLoading(true);
+    try {
+      const response = await fetch(
+        "https://opentdb.com/api.php?amount=5&difficulty=medium&type=multiple"
+      );
+      const questionsArray = await response.json();
+      setQuestions(questionsArray.results);
+    } catch (err) {
+      console.log(err);
+    }
+    setDataLoading(false);
+  }
+
+  if (dataIsLoading) {
+    return <h1>Loading your challenge...</h1>;
+  }
+
+  let questionElements = [];
+  if (questions.length > 0) {
+    questionElements = questions.map((question, index) => {
+      return <Question key={index} data={question} />;
+    });
+  }
+
+  return <div>{questionElements}</div>;
 }
