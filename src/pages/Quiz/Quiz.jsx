9cb4ec60f5687508ed21daa034269e824b2c72c9
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-08-02 22:08:15 +0200

    refactor: Simplified Quiz component
    Removed unnecessary states and conditions and cleaned up code elements

--- src/pages/Quiz/Quiz.jsx
+++ src/pages/Quiz/Quiz.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 import Question from "../../components/Question/Question";
-import getQuestions from "../../components/getQuestions/getQuestions";
+import getQuestions from "../../services/getQuestions";
 import React from "react";
+import Button from "../../components/Button/Button";
 
 export default function Quiz() {
   const [showResults, setShowResults] = useState(false);
-  const [dataHasLoaded, setDataHasLoaded] = useState(false);
   const [dataIsLoading, setDataLoading] = useState(false);
   const [questions, setQuestions] = useState([]);
   const [score, setScore] = useState(0);
@@ -14,68 +14,75 @@ export default function Quiz() {
     async function updateQuestions() {
       setDataLoading(true);
       let questionsArray = await getQuestions();
-      console.log(questionsArray);
       setQuestions(questionsArray);
       setScore(0);
       setDataLoading(false);
     }
-    if (!showResults && !dataHasLoaded) {
-      console.log(dataHasLoaded);
-      setDataHasLoaded(true);
+    if (!showResults) {
       updateQuestions();
     }
-  }, [dataHasLoaded, showResults]);
-
-  // Conditionals
+  }, [showResults]);
 
   if (dataIsLoading) {
     return <h1>Loading your challenge...</h1>;
   }
 
-  let questionElements = [];
-  if (questions.length > 0) {
-    questionElements = questions.map((question, index) => {
-      return (
-        <Question
-          key={index}
-          question={question}
-          indexId={index}
-          showResults={showResults}
-          setScore={setScore}
-        />
-      );
-    });
-  }
+  const questionElements = questions.map((question, index) => {
+    return (
+      <Question
+        key={index}
+        question={question}
+        indexId={index}
+        showResults={showResults}
+        setScore={setScore}
+      />
+    );
+  });
 
   const buttons = !showResults ? (
-    <button
-      onClick={() => {
-        setShowResults(true);
-        setDataHasLoaded(false);
-      }}
-    >
-      Check answers
-    </button>
+    <Button onclick={() => setShowResults(true)} text="Check answers" />
   ) : (
-    <button onClick={() => setShowResults(false)}>Play again</button>
+    <Button onclick={() => setShowResults(false)} text="Play again" />
   );
+
+  let resultText = "";
+  switch (score) {
+    case 0:
+      resultText = "You Suck!";
+      break;
+    case 1:
+      resultText = "Not Great Mister!";
+      break;
+    case 2:
+      resultText = "Closer to the bottom that the top!";
+      break;
+    case 3:
+      resultText = "Pretty Good Gumbo!";
+      break;
+    case 4:
+      resultText = "On your way to the top!";
+      break;
+    case 5:
+      resultText = "It's me Superman!";
+      break;
+    default:
+      resultText = "";
+  }
 
   return (
     <div className="quiz">
-      {questionElements}
-      <div className="footer">
+      <section>
+        <h1 className="quiz-title">Quizzical</h1>
+        {questionElements}
+      </section>
+      <footer className="footer">
         {showResults && (
           <p>
-            {score} out of {questions.length}:{" "}
-            {score < 4
-              ? "You Suck!"
-              : score === 4
-              ? "Almost There!"
-              : "Super Hero!"}
+            {score} out of {questions.length}: {resultText}
           </p>
         )}
         {buttons}
-      </div>
+      </footer>
     </div>
   );
 }
