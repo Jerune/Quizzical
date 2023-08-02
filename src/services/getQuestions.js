62297f0a689e6cb90aaee1589993388e7b7be50b
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-08-02 22:02:28 +0200

    refactor: Moved getQuestions function
    Now in services and not as .jsx file

--- src/services/getQuestions.js
+++ src/services/getQuestions.js
@@ -1,7 +1,7 @@
 export default async function getQuestions() {
   try {
     const response = await fetch(
-      "https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple"
+      "https://opentdb.com/api.php?amount=5&category=11&difficulty=medium&type=multiple"
     );
     const questionsArray = await response.json();
     return questionsArray.results;
